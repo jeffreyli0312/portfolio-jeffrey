57e27a786db165152acc62c5794c2b8a7d7eeac3
jeffreyli0312 <jeffreyli0312@gmail.com>
2023-05-31 13:08:38 -0400

    add assets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,28 +3,60 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.css';
+import React, {useState, useEffect} from "react";
+
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+// import navIcon3 from '../assets/img/nav-icon3.svg';
+// import GithubIcon from '../assets/img/github-icon.svg';
+
 
 function NavBar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={'https://t3.ftcdn.net/jpg/03/02/36/78/360_F_302367825_q15EfpDyyvhal2xrzEL75a8I7yxymcBT.jpg'} alt="Logo" height='60px' style={{borderRadius: "20%"}}/>
+          {/* <img src={logo} alt="Logo" /> */}
+          <h1 style={{color: '#ffffff'}}>JL</h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About Me</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About Me</Nav.Link>
+            <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('experience')}>Experience</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#connect" className={activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('connect')}>Connect</Nav.Link>
           </Nav>
           <span className="navbar-text">
-            <div className="social-icon fa-lg btn-floating">
-            <a href="https://github.com/jeffreyli0312"><img src={'https://cdn-icons-png.flaticon.com/512/25/25231.png'} alt="Github" className='test'/></a>
-            <a href="https://www.linkedin.com/in/jeffreyli0312/"><img src={'https://cdn-icons-png.flaticon.com/512/174/174857.png'} alt="LinkedIn" className='test'/></a>
-            <a href="mailto:j2662li@uwaterloo.com"><img src={'https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png'} alt="Email" className='test'/></a>
+            <div className="social-icon">
+            <a href="https://github.com/jeffreyli0312"><img src={navIcon1} alt="Github" /></a>
+            <a href="https://www.linkedin.com/in/jeffreyli0312/"><img src={navIcon2} alt="LinkedIn"/></a>
+            <a href="mailto:j2662li@uwaterloo.com"><img src={navIcon3} alt="Email"/></a>
             </div>
           </span>
         </Navbar.Collapse>
